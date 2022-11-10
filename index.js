52f58614b2ca5b6d05656512bbc3b3497e6c08ef
angelescobar-20 <66916782+angelescobar-20@users.noreply.github.com>
2022-11-10 00:55:30 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function onSignIn(googleUser) {
     $("#name").text(profile.getName());
     $("#email").text(profile.getEmail());
     $("#image").attr('src',profile.getImageUrl());
-    $("#.data").css("display", "block");
-    $("#.g-signin2").css("display", "none");
+    $(".data").css("display", "block");
+    $(".g-signin2").css("display", "none");
   }
 
   function signOut() {
